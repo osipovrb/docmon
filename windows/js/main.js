98da6a6aa891de9dfbc7f34803852ce8eddd62a2
osipovrb <osipov.rb@yandex.ru>
2021-06-08 12:56:13 +0800

    Improving UX

--- windows/js/main.js
+++ windows/js/main.js
@@ -1,3 +1,14 @@
+import Xel from '../../node_modules/xel/xel.js';
+
+document.body.hidden = true;
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await Xel.whenThemeReady;
+  document.body.hidden = false;
+});
+
+// --------------------------------------------------------------------------
+
 const { ipcRenderer } = require('electron')
 
 document.getElementById('open-form-button').addEventListener('click', () => {
