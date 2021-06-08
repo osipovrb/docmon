647bbbd204ec83a04bf93614007cc37393c58b7b
osipovrb <osipov.rb@yandex.ru>
2021-06-08 12:28:20 +0800

    Windows close fix

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function createWindow (file, width, height) {
             nodeIntegration: true,
             contextIsolation: false,
             defaultEncoding: 'UTF-8',
+            show: false,
         },
     })
     window.loadFile(file)
@@ -17,14 +18,15 @@ function createWindow (file, width, height) {
 }
 
 app.whenReady().then(() => {
-    const mainWindow = createWindow('windows/main.html', 800, 600)
+    let mainWindow = createWindow('windows/main.html', 800, 600)
     ipcMain.on('open-form', (_event, _args) => {
-        const formWindow = createWindow('windows/form.html', 900, 850)
-        formWindow.on('close', (_event) => {
+        let formWindow = createWindow('windows/form.html', 900, 850)
+        formWindow.on('close', (_event) => { 
             mainWindow.webContents.send('main', 'form-closed')
+            formWindow = null 
         })
-        mainWindow.on('close', (_event) => {
-            formWindow.close()
+        mainWindow.on('close', (_event) => { 
+            if (formWindow) formWindow.close() 
         })
     })
 })
